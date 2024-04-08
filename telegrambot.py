6a72b823b225668ae79b1514a7a4c6ebc9567c43
abdulqadircp <abdulqadirs811@gmail.com>
2024-04-08 13:27:35 +0500

    passed the scanned file to telegramgroup

--- telegrambot.py
+++ telegrambot.py
@@ -5,20 +5,13 @@ from helpers import get_report, get_file_id_from_message
 
 
 
-def get_report(report_link=None):
-  headers = {"accept": "application/json",
-            "X-Apikey": KEY_VIRUSTOTAL}
-
-  id = 'ODFiMTg0NWQ0NmJjZWZmOWM5ODY1NWM3YzVlZTlkMjU6MTcxMjQxOTEyOA=='
-  response = requests.get(report_link, headers=headers)
-
-  print("response >>> ",response.text)
-  return response.text
 
 
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 KEY_VIRUSTOTAL = os.getenv("KEY_VIRUSTOTAL")
+BASE_URL = "https://api.telegram.org/bot"
+
 
 # You can set parse_mode by default. HTML or MARKDOWN
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
@@ -26,34 +19,48 @@ bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing?")
+    bot.reply_to(message, "Howdy, how are you doing? \n 1) To Scan The File  Write file  And Upload The File \n 2) To Scan The Link  Write \link And Write The URL  ")
 
 
-@bot.message_handler(content_types=['document', 'audio'])
+@bot.message_handler(content_types= ['document', 'audio','image'])
 def handle_docs_audio(message):
-    str_message = str(message)
-    file_id = get_file_id_from_message(message=str_message)
 
-    get_file_path_api_url = f'https://api.telegram.org/bot{TELEGRAM_TOKEN}/getFile?file_id={file_id}'
+  if message.caption:
+        document = message.document
+        if message.caption.startswith('/scan_file'):
+            bot.reply_to(message, f"Document received for processing! File ID: {document.file_name}")
+        else:
+            bot.reply_to(message, "Please start your document caption with /scan_file")
+  else:
+      bot.reply_to(message, "Please attach a caption to your document starting with /scan_file")
 
-    file_path = requests.get(get_file_path_api_url).json()
+  
+  str_message = str(message)
+  get_file_path_api_url = f'{BASE_URL}{TELEGRAM_TOKEN}/getFile?file_id={message.document.file_id}'
 
-    get_file_api_url = f'https://api.telegram.org/file/bot{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
-    file = requests.get(get_file_api_url)
+  file_path = requests.get(get_file_path_api_url).json()
+  get_file_api_url =  f'{BASE_URL}{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
+  file  = requests.get(get_file_api_url)
 
-    url = "https://www.virustotal.com/api/v3/files"
+  url = "https://www.virustotal.com/api/v3/files"
 
-    files = {"file": file.content}
-    headers = {"accept": "application/json",
-               "X-Apikey": KEY_VIRUSTOTAL}
 
-    response = requests.post(url, files=files, headers=headers)
-    report_link = response.json()["data"]["links"]["self"]
+  files = { "file": file.content}
+  headers = {"accept": "application/json",
+            "X-Apikey": KEY_VIRUSTOTAL}
 
-    report = get_report(report_link)
+  response = requests.post(url, files=files, headers=headers)
+  report_link = response.json()["data"]["links"]["self"]
 
-    print("########### Report ############ ", report)
-    bot.reply_to(report)
+  report= get_report(report_link)
+
+  file_path = f"scan_report_{document.file_name}.txt"
+  with open(file_path, "w") as text_file:
+    text_file.write(report)
+
+  with open(file_path, 'rb') as file:
+    bot.send_document(message.chat.id,file)
+    bot.reply_to(message,f"{document.file_name} scan successfull")
 
 
 bot.infinity_polling()
