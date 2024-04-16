dbf1c845913bb4c7d3728224200657491dd136ff
abdulqadircp <abdulqadirs811@gmail.com>
2024-04-16 06:54:58 +0500

    solved issues : scanned report file naming and scan report queuing

--- helpers.py
+++ helpers.py
@@ -1,17 +1,29 @@
-def get_file_id_from_message(message=None):
-  substring = "file_id"
+def get_content_from_str_dict(message=None,substring="file_id"):
+  message = str(message)
   index = message.find(substring)
   result = message[index:].split(",")[0].split(":")[1].replace("'","").strip()
   return result
-
 
 
 def get_report(report_link=None):
   headers = {"accept": "application/json",
             "X-Apikey": KEY_VIRUSTOTAL}
 
-  id = 'ODFiMTg0NWQ0NmJjZWZmOWM5ODY1NWM3YzVlZTlkMjU6MTcxMjQxOTEyOA=='
+  
   response = requests.get(report_link, headers=headers)
+  flag = True
+  counter = 0
+  json_data = requests.get(report_link, headers=headers).json()
+  while json_data["data"]["attributes"]["status"] == "queued":
+    counter+=1
+    time.sleep(5)
+    json_data = requests.get(report_link, headers=headers).json()
+    if counter == 10:
+      flag = False
+      break
 
-  print("response >>> ",response.text)
-  return response.text
+  if flag:
+    result = response.text
+  else:
+    result = "Failed To Generate The Scan Report [!]"  
+  return result
--- telegrambot.py
+++ telegrambot.py
@@ -1,68 +1,67 @@
 import os
 import requests
 import telebot
-from helpers import get_report, get_file_id_from_message
-
-
-
+import uuid
+from helpers import get_report, get_content_from_str_dict
 
 
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 KEY_VIRUSTOTAL = os.getenv("KEY_VIRUSTOTAL")
 BASE_URL = "https://api.telegram.org/bot"
-
-
-# You can set parse_mode by default. HTML or MARKDOWN
-bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
+bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None) # You can set parse_mode by default. HTML or MARKDOWN
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-    bot.reply_to(message, "Howdy, how are you doing? \n 1) To Scan The File  Write file  And Upload The File \n 2) To Scan The Link  Write \link And Write The URL  ")
+  bot.reply_to(message, "Howdy, how are you doing? \n 1) To Scan The File  Write scan_file  And Upload The File \n 2) To Scan The Link  Write \link And Write The URL  ")
 
 
-@bot.message_handler(content_types= ['document', 'audio','image'])
-def handle_docs_audio(message):
-
+BASE_URL = "https://api.telegram.org/bot"
+@bot.message_handler(content_types= ['document', 'photo', 'audio', 'video', 'voice'])
+def handle_file(message):
+  
   if message.caption:
+        content_type = get_content_from_str_dict(message=message,substring="content_type")
+        file_id = get_content_from_str_dict(message=message,substring="file_id")
+
         document = message.document
         if message.caption.startswith('/scan_file'):
-            bot.reply_to(message, f"Document received for processing! File ID: {document.file_name}")
+            bot.reply_to(message, f"{content_type} received for scanning!")
+            str_message = str(message)
+            get_file_path_api_url = f'{BASE_URL}{TELEGRAM_TOKEN}/getFile?file_id={file_id}'
+
+            file_path = requests.get(get_file_path_api_url).json()
+            get_file_api_url =  f'{BASE_URL}{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
+            file  = requests.get(get_file_api_url)
+
+            url = "https://www.virustotal.com/api/v3/files"
+
+
+            files = { "file": file.content}
+            headers = {"accept": "application/json",
+                      "X-Apikey": KEY_VIRUSTOTAL}
+
+            response = requests.post(url, files=files, headers=headers)
+            print("response .json >>> ", response.json())
+            report_link = response.json()["data"]["links"]["self"]
+            print("report_link >>> ",report_link)
+            time.sleep(5)
+            report= get_report(report_link)
+
+            file_path = f"scan_report_{content_type}_{str(uuid.uuid4())}.txt"
+            with open(file_path, "w") as text_file:
+              text_file.write(report)
+
+            with open(file_path, 'rb') as file:
+              bot.send_document(message.chat.id,file)
+              bot.reply_to(message,f"{content_type} has scanned successfull")
+
         else:
             bot.reply_to(message, "Please start your document caption with /scan_file")
   else:
       bot.reply_to(message, "Please attach a caption to your document starting with /scan_file")
 
-  
-  str_message = str(message)
-  get_file_path_api_url = f'{BASE_URL}{TELEGRAM_TOKEN}/getFile?file_id={message.document.file_id}'
-
-  file_path = requests.get(get_file_path_api_url).json()
-  get_file_api_url =  f'{BASE_URL}{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
-  file  = requests.get(get_file_api_url)
-
-  url = "https://www.virustotal.com/api/v3/files"
-
-
-  files = { "file": file.content}
-  headers = {"accept": "application/json",
-            "X-Apikey": KEY_VIRUSTOTAL}
-
-  response = requests.post(url, files=files, headers=headers)
-  report_link = response.json()["data"]["links"]["self"]
-
-  report= get_report(report_link)
-
-  file_path = f"scan_report_{document.file_name}.txt"
-  with open(file_path, "w") as text_file:
-    text_file.write(report)
-
-  with open(file_path, 'rb') as file:
-    bot.send_document(message.chat.id,file)
-    bot.reply_to(message,f"{document.file_name} scan successfull")
 
 
 bot.infinity_polling()
-
-
