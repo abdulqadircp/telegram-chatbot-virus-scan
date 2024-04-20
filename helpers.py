d3107810f458b4686f5e93caed2daaecdd84c2f8
abdulqadircp <abdulqadirs811@gmail.com>
2024-04-20 11:40:28 +0500

    formatted the code

--- helpers.py
+++ helpers.py
@@ -9,50 +9,48 @@ TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
 KEY_VIRUSTOTAL = os.getenv("KEY_VIRUSTOTAL")
 
 
-def check_report_status(report_link,headers):
-  response = requests.get(report_link, headers=headers)
-  flag = True
-  counter = 0
-  json_data = requests.get(report_link, headers=headers).json()
-  while json_data["data"]["attributes"]["status"] == "queued":
-    counter+=1
-    time.sleep(5)
+def check_report_status(report_link, headers):
+    response = requests.get(report_link, headers=headers)
+    flag = True
+    counter = 0
     json_data = requests.get(report_link, headers=headers).json()
-    if counter == 10:
-      flag = False
-      break
-  return flag , str(json_data)
+    while json_data["data"]["attributes"]["status"] == "queued":
+        counter += 1
+        time.sleep(5)
+        json_data = requests.get(report_link, headers=headers).json()
+        if counter == 10:
+            flag = False
+            break
+    return flag, str(json_data)
 
 
+def get_content_from_str_dict(message=None, substring="file_id"):
+    message = str(message)
+    index = message.find(substring)
+    result = message[index:].split(",")[0].split(":")[
+        1].replace("'", "").strip()
+    return result
 
 
-def get_content_from_str_dict(message=None,substring="file_id"):
-  message = str(message)
-  index = message.find(substring)
-  result = message[index:].split(",")[0].split(":")[1].replace("'","").strip()
-  return result
+def get_file_report(file, api_url=None):
+    files = {"file": file.content}
+    headers = {"accept": "application/json",
+               "X-Apikey": KEY_VIRUSTOTAL}
+
+    response = requests.post(api_url, files=files, headers=headers)
+    report_link = response.json()["data"]["links"]["self"]
+
+    flag, data = check_report_status(report_link, headers)
+
+    if flag:
+        result = data
+    else:
+        result = "Failed To Generate The Scan Report [!]"
+    return result
 
 
-def get_file_report(file ,api_url=None):
-  files = { "file": file.content}
-  headers = {"accept": "application/json",
-            "X-Apikey": KEY_VIRUSTOTAL}
-
-  response = requests.post(api_url, files=files, headers=headers)
-  report_link = response.json()["data"]["links"]["self"]
-
-  flag, data = check_report_status(report_link,headers)
-
-
-  if flag:
-    result = data
-  else:
-    result = "Failed To Generate The Scan Report [!]"
-  return result
-
-
-def get_url_scan_report(api_url ,url):
-    payload = { "url": url }
+def get_url_scan_report(api_url, url):
+    payload = {"url": url}
     headers = {
         "accept": "application/json",
         "x-apikey": KEY_VIRUSTOTAL,
@@ -61,22 +59,17 @@ def get_url_scan_report(api_url ,url):
 
     response = requests.post(api_url, data=payload, headers=headers)
     report_link = response.json()["data"]["links"]["self"]
-    flag = check_report_status(report_link,headers)
+    flag = check_report_status(report_link, headers)
 
-    flag, data = check_report_status(report_link,headers)
-
+    flag, data = check_report_status(report_link, headers)
 
     if flag:
-      result = data
+        result = data
     else:
-      result = "Failed To Generate The Scan Report [!]"
+        result = "Failed To Generate The Scan Report [!]"
     return result
 
 
-def write_text_file(file_path,data):
-  with open(file_path, "w") as text_file:
-    text_file.write(data)
-
-
-
-
+def write_text_file(file_path, data):
+    with open(file_path, "w") as text_file:
+        text_file.write(data)
--- telegrambot.py
+++ telegrambot.py
@@ -4,7 +4,7 @@ import telebot
 import uuid
 import time
 from helpers import get_file_report, get_content_from_str_dict, write_text_file, get_url_scan_report
-from dotenv import load_dotenv, dotenv_values
+from dotenv import load_dotenv
 # loading variables from .env file
 load_dotenv()
 
@@ -16,65 +16,64 @@ BASE_URL = "https://api.telegram.org/bot"
 bot = telebot.TeleBot(TELEGRAM_TOKEN, parse_mode=None)
 
 
-
-
-def send_file_to_group(file_path,message):
-  with open(file_path, 'rb') as file:
-    bot.send_document(message.chat.id,file)
+def send_file_to_group(file_path, message):
+    with open(file_path, 'rb') as file:
+        bot.send_document(message.chat.id, file)
 
 
 @bot.message_handler(commands=['start', 'help'])
 def send_welcome(message):
-  bot.reply_to(message, "Howdy, how are you doing? \n 1) To Scan The File  Write scan_file  And Upload The File \n 2) To Scan The Link  Write scan_url :  And Write The URL  ")
+    bot.reply_to(message, "Howdy, how are you doing? \n 1) To Scan The File  Write scan_file  And Upload The File \n 2) To Scan The Link  Write scan_url :  And Write The URL  ")
 
 
-@bot.message_handler(content_types= ['document', 'photo', 'audio', 'video', 'voice'])
+@bot.message_handler(content_types=['document', 'photo', 'audio', 'video', 'voice'])
 def handle_files(message):
-  if message.caption:
+    if message.caption:
 
         if message.caption.startswith('/scan_file'):
-            content_type = get_content_from_str_dict(message=message,substring="content_type")
-            file_id = get_content_from_str_dict(message=message,substring="file_id")
+            content_type = get_content_from_str_dict(
+                message=message, substring="content_type")
+            file_id = get_content_from_str_dict(
+                message=message, substring="file_id")
             bot.reply_to(message, f"{content_type} received for scanning!")
             str_message = str(message)
             get_file_path_api_url = f'{BASE_URL}{TELEGRAM_TOKEN}/getFile?file_id={file_id}'
 
             file_path = requests.get(get_file_path_api_url).json()
-            get_file_api_url =  f'{BASE_URL}{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
-            file  = requests.get(get_file_api_url)
+            get_file_api_url = f'{BASE_URL}{TELEGRAM_TOKEN}/{file_path["result"]["file_path"]}'
+            file = requests.get(get_file_api_url)
 
             api_url = "https://www.virustotal.com/api/v3/files"
             time.sleep(5)
-            report = get_file_report(file=file,api_url=api_url)
+            report = get_file_report(file=file, api_url=api_url)
             file_name = f"scan_report_{content_type}_{str(uuid.uuid4())}.txt"
-            file_path = os.path.join(SCAN_REPORTS_DIR,file_name)
-            write_text_file(file_path=file_path,data=report)
-            send_file_to_group(file_path=file_path,message=message)
-            bot.reply_to(message,f"{content_type} has scanned successfull")
-
+            file_path = os.path.join(SCAN_REPORTS_DIR, file_name)
+            write_text_file(file_path=file_path, data=report)
+            send_file_to_group(file_path=file_path, message=message)
+            bot.reply_to(message, f"{content_type} has scanned successfull")
 
         else:
 
-            bot.reply_to(message, "Please start your document caption with /scan_file")
-  else:
-      bot.reply_to(message, "Please attach a caption to your document starting with /scan_file")
+            bot.reply_to(
+                message, "Please start your document caption with /scan_file")
+    else:
+        bot.reply_to(
+            message, "Please attach a caption to your document starting with /scan_file")
 
 
-@bot.message_handler(content_types= ['text'])
+@bot.message_handler(content_types=['text'])
 def handle_files(message):
-    if "scan_url :" in  message.text:
+    if "scan_url :" in message.text:
 
-            url = message.text.strip().split("scan_url :")[1]
-            bot.reply_to(message, f"URL {url} received for scanning!")
-            api_url = "https://www.virustotal.com/api/v3/urls"
-            report = get_url_scan_report(api_url=api_url,url=url)
-            file_name = f"scan_report_url_{str(uuid.uuid4())}.txt"
-            file_path = os.path.join(SCAN_REPORTS_DIR,file_name)
-            write_text_file(file_path=file_path,data=report)
-            send_file_to_group(file_path=file_path,message=message)
-            bot.reply_to(message,f"URL has scanned successfull")
-
-
+        url = message.text.strip().split("scan_url :")[1]
+        bot.reply_to(message, f"URL {url} received for scanning!")
+        api_url = "https://www.virustotal.com/api/v3/urls"
+        report = get_url_scan_report(api_url=api_url, url=url)
+        file_name = f"scan_report_url_{str(uuid.uuid4())}.txt"
+        file_path = os.path.join(SCAN_REPORTS_DIR, file_name)
+        write_text_file(file_path=file_path, data=report)
+        send_file_to_group(file_path=file_path, message=message)
+        bot.reply_to(message, f"URL has scanned successfull")
 
 
 bot.infinity_polling()
