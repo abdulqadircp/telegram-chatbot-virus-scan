b6c5eeb15574bad87001eda0198a6ed845c43ade
abdulqadircp <abdulqadirs811@gmail.com>
2024-04-21 14:09:28 +0500

    stored the ip,file and url scanning api response to csv

--- helpers.py
+++ helpers.py
@@ -1,6 +1,8 @@
 import requests
 import time
 import os
+import pandas as pd
+import json
 from dotenv import load_dotenv, dotenv_values
 # loading variables from .env file
 load_dotenv()
@@ -14,14 +16,16 @@ def check_report_status(report_link, headers):
     flag = True
     counter = 0
     json_data = requests.get(report_link, headers=headers).json()
+    print("json_data >> ", json_data)
     while json_data["data"]["attributes"]["status"] == "queued":
         counter += 1
         time.sleep(5)
         json_data = requests.get(report_link, headers=headers).json()
-        if counter == 10:
+
+        if counter == 100:
             flag = False
             break
-    return flag, str(json_data)
+    return flag, json_data
 
 
 def get_content_from_str_dict(message=None, substring="file_id"):
@@ -30,6 +34,23 @@ def get_content_from_str_dict(message=None, substring="file_id"):
     result = message[index:].split(",")[0].split(":")[
         1].replace("'", "").strip()
     return result
+
+
+def get_relevant_data_from_api_response(data, result="results"):
+    # get_relevant_data_from_api_response for File and URL api response
+    print("data >>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>######################################################>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>> \n", type(data), " ", data)
+    merged_dict = {}
+    for key in data["data"]["attributes"][result].keys():
+        anti_virus = data["data"]["attributes"][result][key]
+        # dicts = [dict1, dict2, dict3]
+
+        # Merge dictionaries
+        for key, value in anti_virus.items():
+            if key not in merged_dict:
+                merged_dict[key] = [value]
+            else:
+                merged_dict[key].append(value)
+    return merged_dict
 
 
 def get_file_report(file, api_url=None):
@@ -43,7 +64,9 @@ def get_file_report(file, api_url=None):
     flag, data = check_report_status(report_link, headers)
 
     if flag:
-        result = data
+
+        result = get_relevant_data_from_api_response(data)
+
     else:
         result = "Failed To Generate The Scan Report [!]"
     return result
@@ -64,12 +87,31 @@ def get_url_scan_report(api_url, url):
     flag, data = check_report_status(report_link, headers)
 
     if flag:
-        result = data
+        result = get_relevant_data_from_api_response(data)
     else:
         result = "Failed To Generate The Scan Report [!]"
     return result
 
 
-def write_text_file(file_path, data):
-    with open(file_path, "w") as text_file:
-        text_file.write(data)
+def get_ip_scan_report(api_url):
+
+    headers = {
+        "accept": "application/json",
+        "x-apikey": KEY_VIRUSTOTAL,
+    }
+
+    response = requests.get(api_url, headers=headers)
+    result = response.json()["data"]["attributes"]
+    # print("get_relevant_data_from_api_response >>> ",response.json()["data"]["attributes"])
+    result = get_relevant_data_from_api_response(
+        response.json(), result="last_analysis_results")
+    return result
+
+
+def write_csv_file(file_path, data):
+    # data =json.loads(data)
+
+    df = pd.DataFrame(data)
+    df.to_csv(file_path, index=False)
+    # with open(file_path, "w") as text_file:
+    #     text_file.write(data)
